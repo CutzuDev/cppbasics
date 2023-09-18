05cc1f6d804c86fbce912c01e5123e20ba209911
CutzuDev <alexfarkas05@gmail.com>
2023-09-18 14:47:14 +0300

    temp calculator

--- cppbasics.cpp
+++ cppbasics.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
-#include <cmath>
 
 int main()
 {
+
+    double temp{};
+    char unit{};
+
     const std::string divider{"----------------------------------------------"};
-    char op{};
-    double num1{};
-    double num2{};
-    double result{};
+    std::cout << divider << "\n";
 
-    // NEVER EDIT BELOW
-
-    std::cout << "------------------CALCULATOR------------------"
+    std::cout << "F = Fahrenheit"
               << "\n";
-
-    std::cout << "Choose operator (+ - * /):"
+    std::cout << "C = Celsius"
               << "\n";
-    std::cin >> op;
-
-    std::cout << "Enter first number:"
+    std::cout << "What unit would you like to convert to:"
               << "\n";
-    std::cin >> num1;
+    std::cin >> unit;
+    std::cout << "\n";
 
-    std::cout << "Enter second number:"
-              << "\n";
-    std::cin >> num2;
-
-    switch (op)
+    if (unit == 'F' || unit == 'f')
     {
-    case '+':
-        result = num1 + num2;
-        std::cout << "Result: " << result << "\n";
-        break;
-    case '-':
-        result = num1 - num2;
-        std::cout << "Result: " << result << "\n";
-        break;
-    case '*':
-        result = num1 * num2;
-        std::cout << "Result: " << result << "\n";
-        break;
-    case '/':
-        result = num1 / num2;
-        std::cout << "Result: " << result << "\n";
-        break;
-    default:
-        std::cout << "Your operator sign was invalid"
+
+        std::cout << "Enter temperature in Celsius:"
                   << "\n";
-        break;
+        std::cin >> temp;
+        std::cout << "\n";
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "Temperature is:" << temp << "F\n";
+    }
+    else if (unit == 'C' || unit == 'c')
+    {
+        std::cout << "Enter temperature in Fahrenheit:"
+                  << "\n";
+        std::cin >> temp;
+        std::cout << "\n";
+        temp = (temp - 32) / 1.8;
+        std::cout << "Temperature is:" << temp << "C\n";
+    }
+    else
+    {
+        std::cout << "Invalid input"
+                  << "\n";
     }
 
     std::cout << divider << "\n";
-
     std::cout
         << "\n"
         << "\n";
