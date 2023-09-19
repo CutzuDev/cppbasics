db342ccf82f215aa95cbd68ece9860915d0b3306
CutzuDev <alexfarkas05@gmail.com>
2023-09-19 10:53:27 +0300

    string methodds

--- cppbasics.cpp
+++ cppbasics.cpp
@@ -1,47 +1,18 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
 
-    double temp{};
-    char unit{};
+    std::string name{};
 
     const std::string divider{"----------------------------------------------"};
     std::cout << divider << "\n";
 
-    std::cout << "F = Fahrenheit"
-              << "\n";
-    std::cout << "C = Celsius"
-              << "\n";
-    std::cout << "What unit would you like to convert to:"
-              << "\n";
-    std::cin >> unit;
-    std::cout << "\n";
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
 
-    if (unit == 'F' || unit == 'f')
-    {
-
-        std::cout << "Enter temperature in Celsius:"
-                  << "\n";
-        std::cin >> temp;
-        std::cout << "\n";
-        temp = (1.8 * temp) + 32.0;
-        std::cout << "Temperature is:" << temp << "F\n";
-    }
-    else if (unit == 'C' || unit == 'c')
-    {
-        std::cout << "Enter temperature in Fahrenheit:"
-                  << "\n";
-        std::cin >> temp;
-        std::cout << "\n";
-        temp = (temp - 32) / 1.8;
-        std::cout << "Temperature is:" << temp << "C\n";
-    }
-    else
-    {
-        std::cout << "Invalid input"
-                  << "\n";
-    }
+    std::cout << name.erase(0, 1) << "\n";
 
     std::cout << divider << "\n";
     std::cout
